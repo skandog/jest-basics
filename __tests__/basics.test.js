4574b44da6dc42d68f5639fa080593033496b6a5
skandog <muumrar@gmail.com>
2023-01-31 14:46:41 +0000

    written function tests

--- __tests__/basics.test.js
+++ __tests__/basics.test.js
@@ -10,3 +10,20 @@ test("null", () => {
   expect(i).toBeNull();
   expect(i).toBeDefined();
 });
+
+const animals = ["dog", "cat"];
+
+test("animal array", () => {
+  expect(animals).toContain("cat");
+  expect(animals).toBeInstanceOf(Array);
+});
+
+function getData() {
+  throw new Error("Not Found");
+}
+
+test("getData", () => {
+  expect(() => {
+    getData();
+  }).toThrow("Not Found");
+});

9091284fa689cf01556f95258593422452a6be59
skandog <muumrar@gmail.com>
2023-02-01 13:40:59 +0000

    mocked function, added notes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,16 @@
+import crypto from "crypto";
+
 import getData from "../app.js";
 
+//ok so you mock any 3rd party, rather than actually calling
+// this means you know what to expect
+jest.mock("crypto");
+
 test("fetch data", async () => {
+  // so this is converting the use of crypto, from the actual in
+  // getData, to this expected result of 'bytes'
+  crypto.randomBytes.mockResolvedValueOnce("bytes");
+
   const res = await getData();
   console.log(res);
 });

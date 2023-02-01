b83e88119f63c0456b0a92f1e21cfc624d525d78
skandog <muumrar@gmail.com>
2023-02-01 13:54:14 +0000

    learned about spyOn method from Jest, better than just mock functions

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,16 +1,24 @@
-import crypto from "crypto";
+import crypto, { randomBytes } from "crypto";
 
 import getData from "../app.js";
 
-//ok so you mock any 3rd party, rather than actually calling
-// this means you know what to expect
-jest.mock("crypto");
+// //ok so you mock any 3rd party, rather than actually calling
+// // this means you know what to expect
+// jest.mock("crypto");
+
+// test("fetch data", async () => {
+//   // so this is converting the use of crypto, from the actual in
+//   // getData, to this expected result of 'bytes'
+//   crypto.randomBytes.mockResolvedValueOnce("bytes");
+
+//   const res = await getData();
+//   console.log(res);
+// });
 
 test("fetch data", async () => {
-  // so this is converting the use of crypto, from the actual in
-  // getData, to this expected result of 'bytes'
-  crypto.randomBytes.mockResolvedValueOnce("bytes");
+  jest.spyOn(crypto, "randomBytes").mockResolvedValueOnce("bytes");
 
   const res = await getData();
   console.log(res);
+  expect(res).toBe("bytes");
 });
